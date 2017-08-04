a80469ecc6830139b2ebda75f38f113c36894402
cwza <cwz0205a@gmail.com>
2017-08-04 10:41:53 +0800

    update

--- .circleci/deploy-docker.sh
+++ .circleci/deploy-docker.sh
@@ -1,6 +1,6 @@
 # push to gcloud docker registry
 if [[ ! -z "${CIRCLE_TAG}" ]]; then
-    TAG = echo "$CIRCLE_TAG" | sed 's/^v//g'
+    TAG=$(echo "$CIRCLE_TAG" | sed 's/^v//g')
     echo "push $CIRCLE_TAG $TAG"
 fi
 if [[ "${CIRCLE_BRANCH}" == "master" && -z "${CIRCLE_TAG}" ]]; then
